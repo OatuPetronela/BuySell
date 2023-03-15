527aee1a19b5f7dfb3376f6f35b6e09409fdd021
OatuPetronela <88310964+OatuPetronela@users.noreply.github.com>
2023-03-15 19:25:47 +0200

    added synchronization of login/logout pages

--- src/hooks/useLocalStorageState.js
+++ src/hooks/useLocalStorageState.js
@@ -1,17 +1,21 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const updateStorage = (key, value) => {
-  window.localStorage.setItem(key, JSON.stringify(value));
+  if (window?.localStorage) {
+    window.localStorage.setItem(key, JSON.stringify(value));
+  }
 };
 
 const retriveFromStorage = (key) => {
   const val = window.localStorage.getItem("key");
-  if (val !== null) {
-    try {
-      return JSON.parse(val);
-    } catch (error) {
-      console.error(`Error parsing JSON for key "${key}"`, error);
-      return null;
+  if (window?.localStorage) {
+    if (val !== null) {
+      try {
+        return JSON.parse(val);
+      } catch (error) {
+        console.error(`Error parsing JSON for key "${key}"`, error);
+        return null;
+      }
     }
   }
   return null;
@@ -24,16 +28,39 @@ const useLocalStorageState = (key, initialState) => {
     if (val !== null) {
       return val;
     }
+
     if (initialState !== undefined) {
-      updateStorage(key, initialState);
-      return initialState;
+      let init = initialState;
+      if (typeof initialState === "function") {
+        init = initialState();
+      }
+      updateStorage(key, init);
+      return init;
     }
     return null;
   });
 
+  useEffect(() => {
+    let handleStorageEvent = (e) => {
+      if (e.key === key) {
+        setState(JSON.parse(e.newValue));
+      }
+    };
+
+    window?.addEventListener("storage", handleStorageEvent);
+
+    return () => {
+      window?.removeEventListener("storage", handleStorageEvent);
+    };
+  }, [key]);
+
   const handeleStateUpdate = (newState) => {
-    updateStorage(key, newState);
-    setState(newState);
+    let future = newState;
+    if (typeof newState === "function") {
+      future = newState(state);
+    }
+    updateStorage(key, future);
+    setState(future);
   };
 
   const removeState = () => {
