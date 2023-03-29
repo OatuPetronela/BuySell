f08e6e13df029480eaf34e57e4e9466e48937888
OatuPetronela <88310964+OatuPetronela@users.noreply.github.com>
2023-03-29 20:21:40 +0300

    installed express framework on backend directory

---
+++ backend/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const categories =require('./data/categories')
+
+const app = express;
+
+app.get('/categories', (req, res)=>{
+    res.json(categories)
+})
+
+app.listen(3500, console.log('Server started on port 3500'))
