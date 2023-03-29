34bd2558dddde13bf97c99780d25d8c5b7af07d9
OatuPetronela <88310964+OatuPetronela@users.noreply.github.com>
2023-03-29 21:15:55 +0300

    fetching categories from backend to frontend

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const categories =require('./data/categories')
 
-const app = express;
+const app = express();
 
 app.get('/categories', (req, res)=>{
     res.json(categories)
 })
+
 
 app.listen(3500, console.log('Server started on port 3500'))
--- frontend/src/components/content/ContentMainPage.js
+++ frontend/src/components/content/ContentMainPage.js
@@ -1,157 +1,27 @@
 import { Link } from "react-router-dom";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { ChevronRightIcon } from "@heroicons/react/24/outline";
+import axios from "axios";
 
-
-const categories = [
-  {
-    name: "Auto, moto si ambarcațiuni",
-    hyperlink: "/Auto-moto-ambarcațiuni",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3774/3774278.png",
-    links: [
-      {
-        name: "Autoturisme",
-        url: "/Auto-moto-ambarcațiuni/Autoturisme",
-      },
-      {
-        name: "Autoutilitare",
-        url: "/Auto-moto-ambarcațiuni/Autoutilitare",
-      },
-      {
-        name: "Camioane - Rulote - Remorci",
-        url: "/Auto-moto-ambarcațiuni/Camioane-Rulote-Remorci",
-      },
-      {
-        name: "Motociclete - Scutere - ATV",
-        url: "/Auto-moto-ambarcațiuni/Motociclete-Scutere-ATV",
-      },
-      {
-        name: "Ambarcatiuni",
-        url: "/Auto-moto-ambarcațiuni/Ambarcatiuni",
-      },
-      {
-        name: "Piese auto",
-        url: "/Auto-moto-ambarcațiuni/Piese-auto",
-      },
-      {
-        name: "Utilaje agricole si industriale",
-        url: "/Auto-moto-ambarcațiuni/Utilaje-agricole-si-industriale",
-      },
-    ],
-  },
-  {
-    name: "Piese auto",
-    hyperlink: "/Piese-auto",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3895/3895728.png",
-    links: [
-      {
-        name: "Roti - Jante- Anvelope",
-        url: "/Piese-auto/Roti-Jante-Anvelope",
-      },
-      {
-        name: "Alte piese",
-        url: "/Piese-auto/Alte-piese",
-      },
-      {
-        name: "Piese utilaje agricole",
-        url: "/Piese-auto/Piese-utilaje-agricole",
-      },
-      {
-        name: "Consumabile - accesorii",
-        url: "/Piese-auto/Consumabile-accesorii",
-      },
-      {
-        name: "Piese utilaje industriale",
-        url: "/Piese-auto/Piese-utilaje-industriale",
-      },
-      {
-        name: "Caroserie-Interior",
-        url: "/Piese-auto",
-      },
-      {
-        name: "Mecanica -electrica",
-        url: "/Piese-auto/Mecanica-electrica",
-      },
-    ],
-  },
-  {
-    name: "Imobiliare",
-    hyperlink: "/Imobiliare",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/5101/5101115.png",
-  },
-  {
-    name: "Locuri de muncă ",
-    hyperlink: "/Locuri-de-muncă",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3194/3194461.png",
-  },
-  {
-    name: "Electonice",
-    hyperlink: "/Electonice",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/2777/2777154.png",
-  },
-  {
-    name: "Electrocasnice",
-    hyperlink: "/Electrocasnice",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/1261/1261143.png",
-  },
-  {
-    name: "Casă si grădina",
-    hyperlink: "/Casă-si-grădina",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/1887/1887372.png",
-  },
-  {
-    name: "Mama și copilul",
-    hyperlink: "/Mama-și-copilul",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/1581/1581704.png",
-  },
-  {
-    name: "Sport, arta, activitati",
-    hyperlink: "/Sport-arta-activitati",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3043/3043918.png",
-  },
-  {
-    name: "Animale de companie",
-    hyperlink: "/Animale-de-companie",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3460/3460473.png",
-  },
-  {
-    name: "Agro si Industrie",
-    hyperlink: "/Agro-si-Industrie",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/5341/5341375.png",
-  },
-  {
-    name: "Cazare-Turism",
-    hyperlink: "/Cazare-Turism",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/2560/2560149.png",
-  },
-  {
-    name: "Jocuri-Console",
-    hyperlink: "/Jocuri-Console",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/3097/3097980.png",
-  },
-  {
-    name: "Laptopuri",
-    hyperlink: "/Laptopuri",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/428/428001.png",
-  },
-  {
-    name: "Modă și frumusețe",
-    hyperlink: "/Modă-și-frumusețe",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/1198/1198358.png",
-  },
-  {
-    name: "Servicii și echipamente",
-    hyperlink: "/Servicii-și-echipamente",
-    imageUrl: "https://cdn-icons-png.flaticon.com/512/681/681531.png",
-  },
-];
 
 const ContentMainPage = () => {
   const [openCategory, setOpenCategory] = useState(null);
 
+
   const handleCategoryClick = (index) => {
     setOpenCategory((prevIndex) => (prevIndex === index ? null : index));
   };
+
+  const [categories, setCategories]= useState([])
+
+  useEffect(() => {
+    const fetchCategoires= async () => {
+      const {data}= await axios.get('/categories')
+       setCategories(data)
+    }
+    fetchCategoires()
+  }, [])
+  
 
   return (
     <div className="bg-white">
@@ -165,7 +35,7 @@ const ContentMainPage = () => {
             </div>
             <div className="grid grid-cols-2 mr-8 ml-0 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-8">
               {categories.map((category, index) => (
-                <div key={index} className="text-center">
+                <div key={category.name} className="text-center">
                   <button
                     onClick={() => handleCategoryClick(index)}
                     className="items-center justify-center"
